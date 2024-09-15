d4614d7a75c121e98238ea050f2dbc5faa72e67d
AiRomance <88715229+WHFF521@users.noreply.github.com>
2024-09-15 22:33:09 +0800

    Create 1089-a+b.cpp

---
+++ HDOJ/1089-a+b.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdio>
+#include<map>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<vector>
+using namespace std;
+
+int main(){
+    long long a,b;
+    while(scanf("%lld %lld",&a,&b)!=EOF){
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
