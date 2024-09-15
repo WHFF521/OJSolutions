184b86fb2731aff195e31d9eadd473e99bbebc58
AiRomance <88715229+WHFF521@users.noreply.github.com>
2024-09-15 22:34:33 +0800

    Create 1090-a+b.cpp

---
+++ HDOJ/1090-a+b.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdio>
+#include<map>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<vector>
+using namespace std;
+
+int main(){
+    long long a,b;
+    int T;
+    cin>>T;
+    while(T--){
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
