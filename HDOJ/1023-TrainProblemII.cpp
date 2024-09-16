fee58c3908b0b0f1e5754f4a4d039489d2c6c473
AiRomance <88715229+WHFF521@users.noreply.github.com>
2024-09-16 10:52:00 +0800

    Create 1023-TrainProblemII.cpp

---
+++ HDOJ/1023-TrainProblemII.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cstdio>
+#include<map>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+using namespace std;
+const int mxlen = 1005;
+
+int catalan[105][mxlen];
+// catalan[i][j] stores the j-th digit of the i-th catalan number
+// h(n) = (4n-2)*h(n-1)/(n+1)
+void pre_work(){
+    catalan[1][0] = 1;
+    for(int i=2;i<=100;i++){
+        int tmp = 0 ;
+        int j = 0;
+        for(j=0;j<mxlen;j++){
+            tmp+=catalan[i-1][j]*(4*i-2);
+            catalan[i][j] = tmp%10;
+            tmp/=10;
+        }
+        j=mxlen-1;
+        while(catalan[i][j]==0) j--;
+        while(j>=0){
+            tmp = tmp*10+catalan[i][j];
+            catalan[i][j--] = tmp/(i+1);  
+            if(tmp>=i+1) tmp%=(i+1);
+        }
+    }
+}
+int main(){
+    pre_work();
+    int n;
+    while(scanf("%d",&n)!=EOF){
+        int i = mxlen-1;
+        while(catalan[n][i]==0) i--;
+        while(i>=0) printf("%d",catalan[n][i--]);
+        printf("\n");
+    }
+}
