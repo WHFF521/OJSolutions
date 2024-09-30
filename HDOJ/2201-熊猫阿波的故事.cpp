8e40311c416391c1e17b7e4991fff8aed5c4ebb4
AiRomance <88715229+WHFF521@users.noreply.github.com>
2024-09-30 19:07:44 +0800

    Create 2201-熊猫阿波的故事.cpp

---
+++ HDOJ/2201-熊猫阿波的故事.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdio>
+#include<map>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#define ll long long
+using namespace std;
+const int mxlen = 1e5+5;
+ll n,m;
+double a[mxlen];
+int main(){
+    while(scanf("%lld%lld",&n,&m)!=EOF){
+        printf("%.2lf\n",1.0/(double)n);
+    }
+    return 0;
+}
