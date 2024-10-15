7e29cc64f8c3b924f1e6edd9b41402c0e0c99ddc
AiRomance <88715229+WHFF521@users.noreply.github.com>
2024-10-15 15:11:14 +0800

    Update 扩展欧几里得.cpp

--- 模版和用法/扩展欧几里得.cpp
+++ 模版和用法/扩展欧几里得.cpp
@@ -1,15 +1,13 @@
 #define ll long long
 
 // 扩展欧几里得
-ll exgcd(ll a,ll b,ll &x,ll &y){
-    if(b==0){
-        x = 1;
-        y = 0;
-        return a;
-    }
-    ll r = exgcd(b,a%b,x,y);
-    ll t = x;
-    x = y;
-    y = t-a/b*y;
-    return r;
+// x,y是方程ax+by=gcd(a,b)的解
+//可以用来求逆元，其中x就是逆元，a是原数，b是模数
+void exgcd(int a, int b, int& x, int& y) {
+  if (b == 0) {
+    x = 1, y = 0;
+    return;
+  }
+  exgcd(b, a % b, y, x);
+  y -= a / b * x;
 }
