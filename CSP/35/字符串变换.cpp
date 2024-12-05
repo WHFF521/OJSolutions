22a64087221652e27e85f11cf834b9ca28322586
AiRomance <88715229+WHFF521@users.noreply.github.com>
2024-12-05 16:42:46 +0800

    Create 字符串变换.cpp

---
+++ CSP/35/字符串变换.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<cstdio>
+#include<map>
+#include<vector>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<algorithm>
+#include<iomanip>
+#include<cstring>
+#include<functional>
+#define ll long long
+using namespace std;
+
+void work(string s){
+    vector<string> v;
+
+    map<char,char>mp;
+    int n;
+    cin>>n;
+    string temp="";
+    getline(cin,temp);
+    for(int i=0;i<n;i++){
+        getline(cin,temp);
+        mp[temp[1]]=temp[2];
+    }
+    int m;
+    cin>>m;
+    temp = s;
+    map<string,int> mps;
+    mps[temp] = 1;
+    v.push_back(temp);
+    while(1){
+        for(int i=0;i<temp.length();i++){
+            if(mp[temp[i]]){
+                temp[i] = mp[temp[i]];
+            }
+        }
+        if(mps[temp]==1){
+            break;
+        }
+        v.push_back(temp);
+        mps[temp] = 1;
+    }
+    int pos = 0;
+    for(int i=0;i<v.size();i++){
+        if(v[i]==temp){
+            pos = i;
+            break;
+        }
+    }
+    int len = v.size();
+    int loop = len - pos;
+    
+    for(int i=1;i<=m;i++){
+        int k;
+        cin>>k;
+        if(k<pos){
+            cout<<v[k]<<endl;
+        }else{
+            cout<<v[pos+(k-pos)%loop]<<endl;
+        }
+    }
+}
+int main(){
+    string s;
+    getline(cin,s);
+    work(s);
+    return 0;
+}
